acf7f5d78b4f26b25ac7b05e4b0f00c8e48c0b6d
Pavel Gomarov <p.gomarov@icloud.com>
2021-02-23 21:38:58 +0300

    CollectionView

---
+++ IChat/Controllers/MainTapBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTapBarController.swift
+//  IChat
+//
+//  Created by  Pavel on 23.02.2021.
+//
+
+import UIKit
+
+class MainTapBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listViewController = ListViewController()
+        let peoplesViewController = PeoplesViewController()
+        
+        tabBar.tintColor = #colorLiteral(red: 0.5568627451, green: 0.3529411765, blue: 0.968627451, alpha: 1)
+        let boldConfig = UIImage.SymbolConfiguration(weight: .medium)
+        let convImage = UIImage(systemName: "bubble.left.and.bubble.right", withConfiguration: boldConfig)!
+        let peopleImage = UIImage(systemName: "person.2", withConfiguration: boldConfig)!
+        
+        viewControllers = [
+            generateNavigationController(rootViewController: listViewController, title: "Conversations", image: convImage),
+            generateNavigationController(rootViewController: peoplesViewController, title: "People", image: peopleImage)
+        ]
+        
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        return navigationVC
+    }
+}
---
+++ IChat/Controllers/PeoplesViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PeoplesViewController.swift
+//  IChat
+//
+//  Created by  Pavel on 23.02.2021.
+//
+
+import UIKit
+
+class PeoplesViewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
